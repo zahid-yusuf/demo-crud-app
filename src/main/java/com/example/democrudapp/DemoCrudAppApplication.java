ad2260b4508c5f987aaa842afa08285b841d90c2
zahid-yusuf <tech.zahid.yusuf@gmail.com>
2023-12-12 23:52:14 +0100

    initial commit for spring

---
+++ src/main/java/com/example/democrudapp/DemoCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.democrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/democrudapp/DemoCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
