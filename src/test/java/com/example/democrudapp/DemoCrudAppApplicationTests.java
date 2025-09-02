741e1551878ab6bb68e50f01a15e18ff9bbb3bdb
zahid-yusuf <tech.zahid.yusuf@gmail.com>
2025-09-02 21:50:22 +0200

    fix the build issues

--- src/test/java/com/example/democrudapp/DemoCrudAppApplicationTests.java
+++ src/test/java/com/example/democrudapp/DemoCrudAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.democrudapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoCrudAppApplicationTests {
 
 	@Test
